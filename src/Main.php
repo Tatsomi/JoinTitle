2d2503ca5fcfc94cec1d6caa6424b7f49ce227f8
Tatsomi <TatsomiSobara@gmail.com>
2024-12-10 18:57:57 +0330

    Add files via upload

--- src/Main.php
+++ src/Main.php
@@ -6,8 +6,12 @@ namespace Tatsomi\JoinTitle;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\Config;
+
+use jojoe77777\FormAPI\SimpleForm;
 
 class Main extends PluginBase implements Listener
 {
@@ -23,6 +27,31 @@ class Main extends PluginBase implements Listener
     public function onJoin(PlayerJoinEvent $event): void
     {
         $player = $event->getPlayer();
-        $player->sendTitle($this->config->get("title"));
+        $player->sendTitle($this->config->get("welcometitle"));
+        $player->sendSubtitle($this->config->get("welcomesubtitle"));
+        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($player) {
+            $this->form($player);
+        }), 45);
+    }
+
+    public function form(Player $player): void
+    {
+        $form = new SimpleForm(function (Player $player, $data = null){
+            if ($data === null) {
+                $this->close($player);
+            }
+        });
+        $form->setTitle($this->config->get("formtitle"));
+        $form->setContent($this->config->get("formcontent"));
+        $form->addButton($this->config->get("closebutton"));
+
+        $form->sendToPlayer($player);
+    }
+
+    public function close(Player $player): void
+    {
+        /*
+         * | خالی باشه مشکلی نیست |
+         */
     }
 }
